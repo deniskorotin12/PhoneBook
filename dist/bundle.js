19ff53e7e6f4701864c8cb2f106849a1fe61e4b9
deniskorotin12 <deniskorotin4@gmail.com>
2017-11-17 16:51:05 +0200

    fridayLastCommit

--- dist/bundle.js
+++ dist/bundle.js
@@ -141,17 +141,64 @@ var Surname = document.getElementById("regSurname");
 //     return Users.ContactUsers.push(fullName)
 // }
 
+
 function AddContactButton() {
     var getRegButton = document.getElementById("regButton");
     getRegButton.addEventListener("click", function () {
-        return hideElement.style.visibility = "hidden";
+        return Users.ContactUsers.push("fuck this police"); //hideElement.style.visibility = "hidden";
     });
 };
 
+var grid = document.getElementById('MainTable');
+
+grid.onclick = function (e) {
+    if (e.target.tagName != 'TH') return;
+
+    // Если TH -- сортируем
+    sortGrid(e.target.cellIndex, e.target.getAttribute('data-type'));
+};
+
+function sortGrid(colNum, type) {
+    var tbody = grid.getElementsByTagName('tbody')[0];
+
+    // Составить массив из TR
+    var rowsArray = [].slice.call(tbody.rows);
+
+    // определить функцию сравнения, в зависимости от типа
+    var compare;
+
+    switch (type) {
+        case 'number':
+            compare = function compare(rowA, rowB) {
+                return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+            };
+            break;
+        case 'string':
+            compare = function compare(rowA, rowB) {
+                return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML;
+            };
+            break;
+    }
+
+    // сортировать
+    rowsArray.sort(compare);
+
+    // Убрать tbody из большого DOM документа для лучшей производительности
+    grid.removeChild(tbody);
+
+    // добавить результат в нужном порядке в TBODY
+    // они автоматически будут убраны со старых мест и вставлены в правильном порядке
+    for (var i = 0; i < rowsArray.length; i++) {
+        tbody.appendChild(rowsArray[i]);
+    }
+
+    grid.appendChild(tbody);
+}
+
 AddContactButton();
 hideBlock();
-addNewPerson();
-initTable();
+//addNewPerson();
+//initTable();
 
 /***/ }),
 /* 2 */
